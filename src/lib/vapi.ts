e6506f7410c7f4c53b7613703a8514df56f0ebde
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-11-24 09:38:43 +0530

    Update vapi.ts

--- src/lib/vapi.ts
+++ src/lib/vapi.ts
@@ -1,7 +1,7 @@
 import Vapi from "@vapi-ai/web";
 
 export const ASSISTANT_ID = "30698bd7-1483-49b4-8f61-26c79547a80e";
-export const VAPI_PUBLIC_KEY = import.meta.env.VITE_VAPI_PUBLIC_KEY;
+export const VAPI_PUBLIC_KEY = "939c968a-ebea-40b3-8fe9-451f904f4cd4";
 
 export interface Message {
   role: "user" | "assistant";
